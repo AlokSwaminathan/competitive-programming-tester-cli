2c7cfc394170f857c99476814bdccd56e7769df7
Alok Swaminathan <swaminathanalok@gmail.com>
2023-11-27 14:38:54 -0500

    Rewrote method of getting io for test

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -95,17 +95,7 @@ fn validate_folder(folder: &str) -> Result<PathBuf, String> {
     Ok(folder)
 }
 
-fn validate_usaco_link(link: &str) -> Result<String, String> {
-    if !link.contains(USACO_LINK_PREFIX) {
-        return Err(format!(
-            "Invalid usaco problem link, must be of the form {}<problem id> (Normal problem links follow this format)",
-            USACO_LINK_PREFIX
-        ));
-    }
-    Ok(link.to_string())
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub enum SubmissionType {
     USACO,
     CODEFORCES,
@@ -153,17 +143,84 @@ impl SubmissionData {
     pub fn get_data_link(&self) -> Result<String, String> {
         match self.submission_type {
             SubmissionType::USACO => self.usaco_data_link(),
-            SubmissionType::CODEFORCES => unimplemented!(),
-            SubmissionType::ATCODER => unimplemented!(),
+            _ => unreachable!(),
         }
     }
 
     pub fn get_test_name(&self) -> Result<String, String> {
         match self.submission_type {
             SubmissionType::USACO => self.usaco_test_name(),
-            SubmissionType::CODEFORCES => unimplemented!(),
-            SubmissionType::ATCODER => unimplemented!(),
+            SubmissionType::CODEFORCES => self.codeforces_test_name(),
+            SubmissionType::ATCODER => self.atcoder_test_name(),
         }
+    }
+
+    pub fn get_data(&self) -> Result<PathBuf, String> {
+        match self.submission_type {
+            SubmissionType::ATCODER => self.atcoder_data(),
+            SubmissionType::CODEFORCES => self.codeforces_data(),
+            SubmissionType::USACO => unreachable!(),
+        }
+    }
+
+    pub fn get_io(&self, input_extension: &String, output_extension: &String) -> Result<(IOType, IOType), String> {
+        match self.submission_type {
+            SubmissionType::USACO => self.usaco_io(input_extension, output_extension),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn usaco_io(&self, input_extension: &String, output_extension: &String) -> Result<(IOType, IOType), String> {
+        let problem_page = handle_error!(reqwest::blocking::get(&self.link), format!("Failed to access problem link: {}", self.link));
+        if problem_page.status() != reqwest::StatusCode::OK {
+            return Err(format!(
+                "Failed to access link, status code is not 200 it is {}, link: {} ",
+                problem_page.status(),
+                self.link
+            ));
+        }
+        let problem_page_text = handle_error!(problem_page.text(), "Failed to get HTML from problem page");
+        let io_regex = handle_error!(Regex::new(PROBLEM_IO_REGEX_STR), "Failed to create regex for problem io");
+        let io_match = io_regex.captures(&problem_page_text);
+        let io_match = handle_option!(
+            io_match,
+            "Failed to get io from problem page, page doesn't have \"INPUT FORMAT\" section, could mean ID/Link is invalid"
+        );
+        let io_str = &io_match["io"];
+        let io_str = io_str.trim();
+        let io_string = if io_str == USACO_STANDARD_IO_STR {
+            USACO_STANDARD_IO_STR.to_string()
+        } else {
+            io_str.split(' ').last().unwrap().to_string()
+        };
+
+        let input_io = if io_string == USACO_STANDARD_IO_STR {
+            IOType::STD
+        } else {
+            IOType::FILE(PathBuf::from(&io_string).with_extension(&input_extension))
+        };
+        let output_io = if io_string == USACO_STANDARD_IO_STR {
+            IOType::STD
+        } else {
+            IOType::FILE(PathBuf::from(&io_string).with_extension(&output_extension))
+        };
+        Ok((input_io, output_io))
+    }
+
+    fn atcoder_test_name(&self) -> Result<String, String> {
+        unimplemented!()
+    }
+
+    fn atcoder_data(&self) -> Result<PathBuf, String> {
+        unimplemented!()
+    }
+
+    fn codeforces_test_name(&self) -> Result<String, String> {
+        unimplemented!()
+    }
+
+    fn codeforces_data(&self) -> Result<PathBuf, String> {
+        unimplemented!()
     }
 
     fn usaco_test_name(&self) -> Result<String, String> {
@@ -274,32 +331,6 @@ impl SubmissionData {
     }
 }
 
-fn get_problem_io(link: &String) -> Result<String, String> {
-    let problem_page = handle_error!(reqwest::blocking::get(link), format!("Failed to access problem link: {}", link));
-    if problem_page.status() != reqwest::StatusCode::OK {
-        return Err(format!(
-            "Failed to access link, status code is not 200 it is {}, link: {} ",
-            problem_page.status(),
-            link
-        ));
-    }
-    let problem_page_text = handle_error!(problem_page.text(), "Failed to get HTML from problem page");
-    let io_regex = handle_error!(Regex::new(PROBLEM_IO_REGEX_STR), "Failed to create regex for problem io");
-    let io_match = io_regex.captures(&problem_page_text);
-    let io_match = handle_option!(
-        io_match,
-        "Failed to get io from problem page, page doesn't have \"INPUT FORMAT\" section, could mean ID/Link is invalid"
-    );
-    let io_str = &io_match["io"];
-    let io_str = io_str.trim();
-    let io_string = if io_str == USACO_STANDARD_IO_STR {
-        USACO_STANDARD_IO_STR.to_string()
-    } else {
-        io_str.split(' ').last().unwrap().to_string()
-    };
-
-    Ok(io_string)
-}
 
 impl AddArgs {
     pub fn get_test_data(&self) -> Result<(String, PathBuf, Option<SubmissionData>), String> {
@@ -320,8 +351,20 @@ impl AddArgs {
         } else {
             link.split("/").last().unwrap().split(".").next().unwrap().to_string().clone()
         };
+
+        if submission_data.is_some() && submission_data.as_ref().unwrap().submission_type != SubmissionType::USACO {
+            let data_path = handle_error!(
+                submission_data.as_ref().unwrap().get_data(),
+                format!(
+                    "Failed to get data from link for submission type: {}",
+                    submission_data.unwrap().submission_type
+                )
+            );
+            return Ok((name, data_path, submission_data));
+        }
+
         let link = &if submission_data.is_some() {
-            submission_data.as_ref().unwrap().get_data_link()?
+            handle_error!(submission_data.as_ref().unwrap().get_data_link(), "Failed to get link for test data")
         } else {
             link.clone()
         };
@@ -413,20 +456,26 @@ impl AddArgs {
                 _ => return Err("More than 2 values for --io flag, should be 0-2 values".to_string()),
             };
         } else {
-            let link = if let Some(id) = &self.input_type.usaco_id {
-                Some(format!("{}{}", USACO_LINK_PREFIX, id))
+            let submission_data = if let Some(id) = &self.input_type.usaco_id {
+                let link = format!("{}{}", USACO_LINK_PREFIX, id);
+                SubmissionData::try_from_link(&link)
+            } else if let Some(link) = &self.input_type.link {
+                SubmissionData::try_from_link(link)
             } else {
                 None
             };
-
-            if link.is_some() {
-                let link = link.unwrap();
-                let file_name = get_problem_io(&link)?;
-                if file_name != USACO_STANDARD_IO_STR {
-                    input_io = IOType::FILE(PathBuf::from(&file_name).with_extension(&self.input_extension));
-                    output_io = IOType::FILE(PathBuf::from(&file_name).with_extension(&self.output_extension));
-                }
+            if let Some(submission_data) = submission_data {
+                (input_io, output_io) = submission_data.get_io(&self.input_extension, &self.output_extension)?;
             }
+
+            // if link.is_some() {
+            //     let link = link.unwrap();
+            //     let file_name = get_problem_io(&link)?;
+            //     if file_name != USACO_STANDARD_IO_STR {
+            //         input_io = IOType::FILE(PathBuf::from(&file_name).with_extension(&self.input_extension));
+            //         output_io = IOType::FILE(PathBuf::from(&file_name).with_extension(&self.output_extension));
+            //     }
+            // }
         }
         println!("Test IO: {:?}, {:?}", input_io, input_io);
 
